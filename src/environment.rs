6cf7ae29e458ad7ada95c80aa452a765e9d7f043
Vibhav Pant <vibhavp@gmail.com>
2016-05-11 22:13:41 +0530

    Move env to environment.rs

---
+++ src/environment.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+use std::result::Result;
+use std::option::Option;
+use types::HeapObject;
+use error::Err;
+
+pub struct Environment(Vec<HashMap<String, HeapObject>>);
+
+impl Environment {
+    pub fn new() -> Environment {
+        let mut e = Environment(Vec::with_capacity(1));
+        e.push();
+        e
+    }
+
+    #[inline(always)]
+    pub fn push(&mut self) {
+        self.0.push(HashMap::<String, HeapObject>::new());
+    }
+
+    #[inline(always)]
+    pub fn pop(&mut self) {
+        self.0.pop().expect("popping the root environment");
+    }
+
+    #[inline(always)]
+    pub fn insert_sym(&mut self, name: String, value: HeapObject) {
+        self.0.last_mut().unwrap().insert(name, value);
+    }
+
+    pub fn find_sym(&self, name: String) -> Result<&HeapObject, Err> {
+        for i in self.0.len()-1..0 {
+            if let Option::Some(val) = self.0[i].get(&name) {
+                return Result::Ok(val)
+            }
+        }
+
+        Result::Err(Err::SymbolNotFound(name))
+    }
+
+    pub fn mark_all(&mut self) {
+        for env in self.0.iter_mut() {
+            for (_, object) in env {
+                object.borrow_mut().mark();
+            }
+        }
+    }
+}
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,27 +1,25 @@
-use types::{Object, Type, HeapObject, list};
-use err::Err;
+use types::{Object, Type, HeapObject, Lambda, Procedure, List};
+use error::Err;
+use environment::Environment;
 
-use std::collections::HashMap;
 use std::option::Option;
 use std::result::Result;
 use std::rc::Rc;
 use std::cell::RefCell;
-use std::collections::LinkedList;
 
 pub struct Interpreter {
-    live_objects: Box<Vec<HeapObject>>,
-    environment: Box<Vec<HashMap<String, HeapObject>>>,
+    live_objects: Vec<HeapObject>,
+    environment: Environment,
+    nil: HeapObject,
 }
 
 impl Interpreter {
     pub fn new() -> Self {
-        let mut i = Interpreter{
-            live_objects: Box::new(Vec::new()),
-            environment: Box::new(Vec::with_capacity(1)),
-        };
-
-        i.env_push();
-        return i;
+        Interpreter{
+            live_objects: Vec::new(),
+            environment: Environment::new(),
+            nil: Rc::new(RefCell::new(Box::new(Object::new(Type::Nil))))
+        }
     }
 
     pub fn new_object(&mut self, t: Type) -> HeapObject {
@@ -30,58 +28,86 @@ impl Interpreter {
         obj
     }
 
-    fn env_push(&mut self) {
-        let hash = HashMap::<String, HeapObject>::new();
-        self.environment.push(hash);
-    }
-
-    fn env_pop(&mut self) {
-        match self.environment.pop() {
-            Some(val) => val,
-            None => panic!("root environment removed, this shouldn't happen"),
-        };
-    }
-
-    fn insert_symbol(&mut self, name: String, value: HeapObject) {
-        let index = self.environment.len()-1;
-        self.environment[index].insert(name, value);
-    }
-
-    // insert symbol in last to last env
-    fn insert_symbol_prev(&mut self, name: String, value: HeapObject) {
-        let index = self.environment.len()-2;
-        self.environment[index].insert(name, value);
-    }
-
-    fn find_symbol(&self, name: String) -> Option<&HeapObject> {
-        let index = self.environment.len()-1;
-
-        self.environment[index].get(&name)
-    }
-
-    fn find_symbol_prev(&self, name: String) -> Option<&HeapObject> {
-        let index = self.environment.len()-2;
-
-        self.environment[index].get(&name)
-    }
-
-    fn mark_all(&mut self) {
-        for env in self.environment.iter_mut() {
-            Object::mark_environment(env);
-        }
-    }
-
     fn gc(&mut self) -> u64 {
         let mut count = 0 as u64;
-        self.mark_all();
+        self.environment.mark_all();
 
         for i in 0..self.live_objects.len() {
-            if !self.live_objects[i].borrow_mut().marked {
+            if !self.live_objects[i].borrow().marked {
                 self.live_objects.swap_remove(i);
                 count += 1;
+                continue;
+            }
+
+            self.live_objects[i].borrow_mut().marked = false;
+        }
+
+        count
+    }
+
+    fn eval_lambda(&mut self, lambda: &Lambda, exp: List) -> Result<HeapObject, Err> {
+        let params = lambda.params.borrow().unwrap_list().clone();
+        if params.len() != exp.len() - 1 {
+            return Result::Err(Err::WrongArgsNum{wanted: params.len(), got: exp.len()-1});
+        }
+
+        let mut closure = false;
+
+        let mut last = self.nil.clone();
+
+        // (lambda (a r g s) body)
+        for obj in exp.iter().skip(2) {
+            let res = self.eval(obj.clone());
+            match res {
+                Result::Ok(obj) => last = obj,
+                Result::Err(err) => {
+                    if closure {
+                        self.environment.pop();
+                    };
+                    return Result::Err(err)
+                }
             }
         }
-        count
+
+        if closure {
+            self.environment.pop();
+        }
+
+        Result::Ok(last)
+    }
+
+    fn eval_cons(&mut self, c: &List) -> Result<HeapObject, Err> {
+        let frontopt = c.front();
+
+        if let Option::None = frontopt {
+            return Result::Err(Err::EmptyList);
+        }
+
+        let front = self.eval(frontopt.unwrap().clone());
+
+        match front {
+            Result::Ok(frontval) => {
+                match frontval.borrow().object_type {
+                    Type::Procedure(ref p) => match p.as_ref() {
+                        &Procedure::Primitive(prim) => prim(c),
+                        &Procedure::Lambda(ref lambda) => self.eval_lambda(lambda, c.clone())
+                    },
+                    _ => Result::Err(Err::NotCallable(frontval.borrow().get_type_string()))
+                }
+            },
+            Result::Err(e) => Result::Err(e)
+        }
+    }
+
+    pub fn eval(&mut self, hobj: HeapObject) -> Result<HeapObject, Err> {
+        match hobj.borrow().object_type {
+            Type::Cons(ref c) => self.eval_cons(c.as_ref()),
+            Type::Symbol(ref sym) => {
+                let val = try!(self.environment.find_sym(sym.clone()));
+                Result::Ok(val.clone())
+            },
+            _ => Result::Ok(hobj.clone()),
+        }
     }
 }
 
@@ -91,27 +117,21 @@ mod test {
     use super::*;
     use types::Type;
     use std::string::ToString;
-    use std::rc::Rc;
 
     #[test]
     fn test_gc() {
         let mut interpreter = Interpreter::new();
 
-        {
-            {
-                let mut obj = interpreter.new_object(Type::String("foobar".to_string()));
-                interpreter.insert_symbol("test".to_string(), obj.clone());
-            }
-            assert_eq!(interpreter.gc(), 0);
-
-            let mut obj = interpreter.new_object(Type::String("foobar".to_string()));
-            assert_eq!(interpreter.gc(), 1);
-            assert_eq!(interpreter.gc(), 0);
-        }
-
-        for obj in interpreter.live_objects.into_iter() {
-            assert_eq!(Rc::strong_count(&obj), 2);
-            assert_eq!(Rc::weak_count(&obj), 0);
-        }
+        let obj = interpreter.new_object(Type::String("foobar".to_string()));
+        interpreter.environment.push();
+        interpreter.environment.insert_sym("test".to_string(), obj.clone());
+        assert_eq!(interpreter.gc(), 0);
+        interpreter.environment.pop();
+        assert_eq!(interpreter.gc(), 1);
+        
+        interpreter.new_object(Type::String("foobar".to_string()));
+        assert_eq!(interpreter.gc(), 1);
+        assert_eq!(interpreter.gc(), 0);
+       
     }
 }
